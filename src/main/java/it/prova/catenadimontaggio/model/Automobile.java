c3d0d7225375874b559e91c1ee22536b72673b77
Cristian <cristianma98@gmail.com>
2022-06-07 17:06:23 +0200

    Commit di inizio progetto

---
+++ src/main/java/it/prova/catenadimontaggio/model/Automobile.java
@@ -0,0 +1,15 @@
+package it.prova.catenadimontaggio.model;
+
+import java.util.Date;
+
+public class Automobile {
+
+	private Long id;
+
+	private String modello;
+
+	private String telaio;
+
+	private Date dataProduzione;
+
+}
---
+++ src/main/java/it/prova/catenadimontaggio/model/SlotCatenaDiMontaggio.java
@@ -0,0 +1,16 @@
+package it.prova.catenadimontaggio.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlotCatenaDiMontaggio {
+
+	private Long id;
+
+	private String brand;
+
+	private String country;
+
+	private List<Automobile> automobili = new ArrayList<Automobile>();
+
+}
