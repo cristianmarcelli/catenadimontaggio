e8c4ff69fc8b93e70fb503fe7d5ef0df4fa4c10f
Cristian <cristianma98@gmail.com>
2022-06-07 17:10:49 +0200

    Completato model

--- src/main/java/it/prova/catenadimontaggio/model/Automobile.java
+++ src/main/java/it/prova/catenadimontaggio/model/Automobile.java
@@ -12,4 +12,60 @@ public class Automobile {
 
 	private Date dataProduzione;
 
+	public Automobile() {
+	}
+
+	public Automobile(Long id, String modello, String telaio, Date dataProduzione) {
+		super();
+		this.id = id;
+		this.modello = modello;
+		this.telaio = telaio;
+		this.dataProduzione = dataProduzione;
+	}
+
+	public Automobile(Long id, String modello, String telaio) {
+		super();
+		this.id = id;
+		this.modello = modello;
+		this.telaio = telaio;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getModello() {
+		return modello;
+	}
+
+	public void setModello(String modello) {
+		this.modello = modello;
+	}
+
+	public String getTelaio() {
+		return telaio;
+	}
+
+	public void setTelaio(String telaio) {
+		this.telaio = telaio;
+	}
+
+	public Date getDataProduzione() {
+		return dataProduzione;
+	}
+
+	public void setDataProduzione(Date dataProduzione) {
+		this.dataProduzione = dataProduzione;
+	}
+
+	@Override
+	public String toString() {
+		return "Automobile [id=" + id + ", modello=" + modello + ", telaio=" + telaio + ", dataProduzione="
+				+ dataProduzione + "]";
+	}
+
 }
--- src/main/java/it/prova/catenadimontaggio/model/SlotCatenaDiMontaggio.java
+++ src/main/java/it/prova/catenadimontaggio/model/SlotCatenaDiMontaggio.java
@@ -13,4 +13,59 @@ public class SlotCatenaDiMontaggio {
 
 	private List<Automobile> automobili = new ArrayList<Automobile>();
 
+	public SlotCatenaDiMontaggio() {
+	}
+
+	public SlotCatenaDiMontaggio(Long id, String brand, String country, List<Automobile> automobili) {
+		super();
+		this.id = id;
+		this.brand = brand;
+		this.country = country;
+		this.automobili = automobili;
+	}
+
+	public SlotCatenaDiMontaggio(Long id, String brand, String country) {
+		super();
+		this.id = id;
+		this.brand = brand;
+		this.country = country;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public List<Automobile> getAutomobili() {
+		return automobili;
+	}
+
+	public void setAutomobili(List<Automobile> automobili) {
+		this.automobili = automobili;
+	}
+
+	@Override
+	public String toString() {
+		return "SlotCatenaDiMontaggio [id=" + id + ", brand=" + brand + ", country=" + country + "]";
+	}
+
 }
